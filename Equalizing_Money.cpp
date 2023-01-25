3b1cdfcb0cdbfb20a3000d6d809bf8df67838daf
ftanvir <ft.tanvir551@gmail.com>
2023-01-25 12:03:08 +0600

    problem solvedd

--- Equalizing_Money.cpp
+++ Equalizing_Money.cpp
@@ -1,142 +1,60 @@
-//LightOJ problem
-
+// //LightOJ problem
 #include <bits/stdc++.h>
 using namespace std;
-
-/**----data type----*/
-#define ll long long int
-#define llu unsigned long long int
-
-#define fast      ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-#define pi          acos(-1.0)
-#define nl          '\n'
-/**-------Shortend Library-------*/
-#define vi vector<int>
-#define mii map<int, int>
-#define f(i,n) for(ll i=0; i<n; i++)
-#define FOR(i, a, b) for(ll i=a; i<=b; i++)
-#define testcase ll t;cin>>t;while(t--)
-
-#define ff first
-#define ss second
-#define all(a)         a.begin(),a.end()
-#define Sort(x)        sort(x.begin(),x.end())
-#define SortR(x)       sort(x.begin(),x.end(),greater<int>())
-#define Reverse(x)     reverse(x.begin(),x.end())
-
-#define SortA(ar,s)    sort(ar,ar+s)
-#define SortD(ar,s)    sort(ar,ar+s,greater<int>())
-
-#define gcd(a,b)       __gcd(a,b)
-#define lcm(a,b)       (a*(b/gcd(a,b)))
-#define sq(x)          (x)*(x)
-#define mid(l,r)       (l+(r-l)/2)
-
-#define leftShift(p)         (p<<1)
-#define rightShift(p)        (p>>1)
-
-#define min3(a,b,c)       min(a,min(b,c))
-#define min4(a,b,c,d)     min(a,min(b,min(c,d)))
-#define max3(a,b,c)       max(a,max(b,c))
-#define max4(a,b,c,d)     max(a,max(b,max(c,d)))
-#define pb(x)             push_back(x)
-#define mod               1e8+7
-#define precision(x)      cout<<fixed<<setprecision(x)
-#define yes               cout<<"YES"<<nl
-#define no                cout<<"NO"<<nl
-
-/**----Functions to use----**/
-ll powerLog(ll base, ll power)
-{
-    ll res = 1;
-    while(power)
-    {
-        if(power%2)
-        {
-            res *= base;
-            power--;
-        } else 
-        {
-            base *=base;
-            power/=2;
+const int N = 1e3 + 5;
+vector <int> adj[N];
+bool vis[N];
+int sz, ttl;
+int a[N];
+void dfs(int node) {
+    vis[node] = 1;
+    sz++;
+    ttl += a[node];
+    for (int i = 0; i < adj[node].size(); i++) {
+        int chld = adj[node][i];
+        if (!vis[chld]) {
+            dfs(chld);
         }
     }
-    return res;
 }
-
-const int N = 1e5+10;
-// vector<int>g[N];
-// bool vis[N];
-
-void dfs(int s, vector<int>& g[], int & vis[]) {
-
-    vis[s] = true;
-
-    for(auto child:g[s]) {
-        if(!vis[child]) {
-            dfs(child, g, vis);
-        }
-    }
-
-}
-
-void cholokori()
-{
+void tc(int cs) {
+    memset(vis, 0, sizeof(vis));
     int n, m;
-    cin>>n>>m;
-
-    int sum = 0;
-    for(int i=0; i<n; i++) {
-        int num;
-        cin>>num;
-        sum+=num;
-    }
-    if(sum%n != 0) {
-        cout<<"No"<<endl;
-        return;
-    }
-    
-    // g->clear();
-    // for(int i=0; i<=n; i++) {
-    //     vis[i] = false;
-    // }
-
-    vector<int>g[N];
-    bool vis[N];   
-    memset(vis, false, sizeof(vis));
-    while(m--) {
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++) cin >> a[i];
+    for (int i = 0; i < m; i++) {
         int u, v;
-        cin>>u>>v;
-        g[u].push_back(v);
-        g[v].push_back(u);
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
     }
-
-    dfs(1, g, vis);
-
-    bool chk = true;
-    for(int i=1; i<=n; i++) {
-        if(!vis[i]) {
-            chk = false;
+    vector <int> v;
+    bool flg = 1;
+    for (int i = 1; i <= n; i++) {
+        if (!vis[i]) {
+            sz = 0, ttl = 0;
+            dfs(i);
+            if (ttl % sz) {
+                flg = 0;
+                break;
+            }
+            else v.push_back(ttl / sz);
+        }
+    }
+    for (int i = 1; i < v.size(); i++) {
+        if (v[i] != v[i-1]) {
+            flg = 0;
             break;
-        } 
+        }
     }
-
-    if(chk) {
-        cout<<"Yes"<<endl;
-    } else {
-        cout<<"No"<<endl;
-    }
-
+    cout << "Case " << cs << ": ";
+    (flg) ? cout << "Yes\n" : cout << "No\n";
+    for (int i = 1; i <= n; i++)adj[i].clear();
 }
-
-int tc = 1;
-int main()
-{
-    testcase{
-        cout<<"Case "<<tc++<<": ";
-        // g->clear();
-        // memset(vis, false, sizeof(vis));
-        cholokori();
+int main() {
+    int t, cs = 0;
+    cin >> t;
+    while (t--) {
+        tc(++cs);
     }
-    return 0;
 }
